c9c79963b8553b1c09f949587a2cf011aac22d8d
Shr4528 <Shr4528@gmail.com>
2023-05-31 13:48:49 -0500

    home

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/scenes/navbar';
+import Home from '@/scenes/home';
 import { useEffect, useState } from 'react';
 import { SelectedPage } from '@/shared/types';
 
@@ -25,8 +26,9 @@ function App() {
       <Navbar
         isTopOfPage={isTopOfPage}
       selectedPage={selectedPage}
-      setSelectedPage={setSelectedPage}
-    />
+       setSelectedPage={setSelectedPage}
+      />
+      <Home  setSelectedPage={setSelectedPage}/>
     </div>
   );
 }
