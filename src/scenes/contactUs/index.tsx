ee618e267b87d5bce5bf401c6f17e019f2ea6662
Shr4528 <Shr4528@gmail.com>
2023-06-10 10:38:03 -0500

    contact

---
+++ src/scenes/contactUs/index.tsx
@@ -0,0 +1,84 @@
+import { SelectedPage } from '@/shared/types';
+import { useForm } from 'react-hook-form';
+import { motion } from 'framer-motion';
+import ContactUsPageGraphic from '@/assets/ContactUsPageGraphic.png';
+import HText from '@/shared/HText';
+
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void;
+};
+
+const ContactUs = ({ setSelectedPage }: Props) => {
+
+    const { register,
+        trigger,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = async (e: any) => { 
+        const isValid = await trigger();
+        if (!isValid) {
+            e.preventDefault();
+        }
+        console.log(e);
+    }
+
+    return (
+        <section id='contactus' className='mx-auto w-5/6 pt-24 pb-32'>
+            <motion.div
+                onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}>
+                
+                {/* HEADER */}
+
+                <motion.div
+                    className='md:w-3/5'
+                    initial='hidden'
+                    whileInView='visible'
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.5 }}
+                    variants={{
+                      hidden: { opacity: 0, x: -50 },
+                      visible: { opacity: 1, x: 0 },
+                    }}>
+                    <HText>
+                        <span className='text-primary-500'> JOIN NOW</span> TO GET STARTED
+                    </HText>
+                    <p className='my-5'>
+                        Congue adipiscing risus commodo placerat. Tellus et in feugiat nisl
+                        sapien vel rhoncus. Placerat at in enim pellentesque. Nulla
+                        adipiscing leo egestas nisi elit risus sit. Nunc cursus sagittis.
+                    </p>
+                </motion.div>
+
+                {/* FORM & IMAGE */}
+
+                <div className='mt-10 justify-between gap-8 md:flex'>
+                    <motion.div
+                        className='mt-10 basis-3/5md:mt-0'
+                        initial='hidden'
+                        whileInView='visible'
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ duration: 0.5 }}
+                        variants={{
+                          hidden: { opacity: 0, y: 50 },
+                          visible: { opacity: 1, y: 0 },
+                        }}
+                    >
+                        <form
+                            target='_blank'
+                            onSubmit={onSubmit}
+                            method='POST'
+                            //action="https://formsubmit.co/e8a5bdfa807605332f809e5656e27c6e"
+                        >
+                            
+                        </form>
+                    </motion.div>
+                </div>
+            </motion.div>
+        </section>
+    )
+   
+};
+
+export default ContactUs;
